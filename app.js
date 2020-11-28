9e57f1cbd69ddc31c529af84bb11255403c1e678
Juan <jd.berbeo@gmail.com>
2020-11-27 21:06:05 -0500

    corrijo iteración

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const hbs = require('express-handlebars')
 let array = []
 
 
-    for (let i = 1; i<50; i++){
+    for (let i = 1; i<=50; i++){
         if (i%2==0) {
             array[i] = i + " Soy Par!"
         }
